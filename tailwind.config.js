ead81efc47dfd4ca89d47b1a052f085c87a4d284
vdloc <uvcudlco@gmail.com>
2022-09-22 14:39:41 +0700

    update tailwind purge synstax

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,10 @@ function withOpacity(variableName) {
 
 module.exports = {
   mode: "jit",
-  purge: [
+  content: [
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       screens: {
